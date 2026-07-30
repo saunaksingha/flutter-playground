c881f498c4b213bd409d7a32b39a2180a96229d9
Saunak Singha <singhasaunak@gmail.com>
2026-07-30 09:15:10 +0530

    feat: implement expense creation and listing

--- 1. Expense-Tracker/expense_tracker/lib/expenses.dart
+++ 1. Expense-Tracker/expense_tracker/lib/expenses.dart
@@ -1,5 +1,6 @@
 import 'package:expense_tracker/widget/expenses_list.dart';
 import 'package:expense_tracker/models/expense.dart';
+import 'package:expense_tracker/widget/new_expense.dart';
 import 'package:flutter/material.dart';
 
 class Expenses extends StatefulWidget {
@@ -10,6 +11,16 @@ class Expenses extends StatefulWidget {
 }
 
 class _ExpensesState extends State<Expenses> {
+  void _openAddExpenseOverlay() {
+    showModalBottomSheet(
+      isScrollControlled: true,
+      context: context,
+      builder: (ctx) {
+        return NewExpense(onAddExpense: _addExpense);
+      },
+    );
+  }
+
   final List<Expense> _registeredExpenses = [
     Expense(
       title: "Biriyani",
@@ -26,9 +37,24 @@ class _ExpensesState extends State<Expenses> {
     ),
   ];
 
+  void _addExpense(Expense expense) {
+    setState(() {
+      _registeredExpenses.add(expense);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text("Expense Tracker"),
+        actions: [
+          IconButton(
+            onPressed: _openAddExpenseOverlay,
+            icon: const Icon(Icons.add),
+          ),
+        ],
+      ),
       body: Column(
         children: [
           const Text("The chart"),
