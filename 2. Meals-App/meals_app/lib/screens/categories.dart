6de38c453bbc29555f1847d5f91d21b03faddebc
Saunak Singha <singhasaunak@gmail.com>
2026-07-31 10:15:03 +0530

    chore: initialize Meals App project

---
+++ 2. Meals-App/meals_app/lib/screens/categories.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class Categories extends StatelessWidget {
+  const Categories({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Pick your category")),
+      body: GridView(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: [],
+      ),
+    );
+  }
+}
---
+++ 2. Meals-App/meals_app/lib/widgets/category_grid_item.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/models/category.dart';
+
+class CategoryGridItem extends StatelessWidget {
+  const CategoryGridItem({super.key, required this.category});
+
+  final Category category;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            category.color.withAlpha(140),
+            category.color.withAlpha(230),
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: Text(
+        category.title,
+        style: Theme.of(context).textTheme.titleLarge!.copyWith(
+          color: Theme.of(context).colorScheme.onSurface,
+        ),
+      ),
+    );
+  }
+}
